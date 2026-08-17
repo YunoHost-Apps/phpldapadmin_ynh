e541b1204d557ba7f34f97362b38a52cb797e2ae
Alexandre Aubin <4533074+alexAubin@users.noreply.github.com>
2026-08-17 22:20:00 +0200

    Testing / 2.2.1 / drop support for subpath installs (#46)
    * Update manifest.toml: fix autoupdater
    
    * Auto-update READMEs
    
    * Fix (#41)
    
    * fix
    
    * key
    
    * Update install
    
    * Update install
    
    * cleaning
    
    * bump
    
    * Auto-update READMEs
    
    * fix
    
    * Update tests.toml
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * Simplify (#43)
    
    * fix
    
    * key
    
    * Update install
    
    * Update install
    
    * cleaning
    
    * bump
    
    * Auto-update READMEs
    
    * fix
    
    * Update tests.toml
    
    * Update screenshot.png
    
    * Update install
    
    * Update upgrade
    
    * Update upgrade
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * Improve app short description
    
    * Auto-update READMEs
    
    * cleaning
    
    * Auto-update READMEs
    
    * [autopatch] Automatic patch to simplify `proxy_pass`/`fastcgi_pass` options in nginx.conf (PLEASE DOUBLE CHECK BEFORE MERGING) (#47)
    
    Co-authored-by: Yunohost-Bot <>
    
    * Update manifest.toml
    
    * Update install
    
    ---------
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    Co-authored-by: eric_G <46165813+ericgaspar@users.noreply.github.com>
    Co-authored-by: YunoHost Bot <yunohost-bot@users.noreply.github.com>

--- scripts/_common.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#=================================================
-# COMMON VARIABLES AND CUSTOM HELPERS
-#=================================================
-
-composer_version="2.8.9"
-nodejs_version="22"
